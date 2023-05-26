347fe8795232981b8529d1729249f4a6b8b85d93
Jojobas <imalahov30@gmail.com>
2023-05-26 15:03:34 +0500

    fixed TaskSerializer and import in views.py

--- main/views.py
+++ main/views.py
@@ -3,23 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework import viewsets
 from .models import User, Task, Tag
-from serializers import UserSerializer, TagSerializer, TaskSerializer
+from .serializers import UserSerializer, TagSerializer, TaskSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all().order_by("id")
+    queryset = User.objects.order_by("id")
     serializer_class = UserSerializer
 
 
 class TagViewSet(viewsets.ModelViewSet):
-    queryset = Tag.objects.all().order_by("id")
+    queryset = Tag.objects.order_by("id")
     serializer_class = TagSerializer
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = (
-        Task.objects.all()
-        .select_related("author", "assignee")
+        Task.objects.select_related("author", "assignee")
         .prefetch_related("tags")
         .order_by("id")
     )
