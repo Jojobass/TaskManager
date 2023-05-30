e24cc945985801e5b5bad42023c0d77d16e5926c
Jojobas <imalahov30@gmail.com>
2023-05-30 11:47:12 +0500

    made default swagger

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     "main.apps.MainConfig",
     "rest_framework",
     "django_filters",
+    "drf_yasg",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
